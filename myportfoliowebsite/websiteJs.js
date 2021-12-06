a3be28aec4687747006586854d0094d81dee7364
prareD <alluprathapreddy@gmail.com>
2021-12-06 13:30:26 +0530

    nothing new

---
+++ myportfoliowebsite/websiteJs.js
@@ -0,0 +1,10 @@
+function btnonclick()
+{
+    alert("Welcome to my website");
+    document.body.style.backgroundColor='cyan';
+}
+function dateload()
+{
+    var datetodayj = new Date();
+    document.getElementById('datetoday').innerHTML = datetodayj;
+}
